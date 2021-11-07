8a48185571c185f14bd0fd425f2b916dc19bf2e2
MichalPlatko <miso.platko@gmail.com>
2021-11-07 20:10:42 +0100

    main file that initiliazes everything

--- Game/scripts/main.js
+++ Game/scripts/main.js
@@ -1,31 +1,18 @@
-import {CircleCollider, PolygonCollider, RectangleCollider} from "./PhysicsObjects.js";
-import Canvas from "./Canvas.js";
+import Renderer from "./Renderer.js";
+import Player from "./Player.js";
 
-window.onload=Canvas.init();//inits canvas after whole page loads
-const ctx=Canvas.ctx;
 // module aliases
 let Engine = Matter.Engine,
+    Vector = Matter.Vector,
     Runner = Matter.Runner;
 // create an engine
 let engine = Engine.create();
+//disable global gravity
+engine.world.gravity.y=0;
 // create runner
 let runner = Runner.create();
 // run the engine
 Runner.run(runner, engine);
 
-
-const boxR1=new RectangleCollider(engine.world,Matter.Vector.create(375,200),Matter.Vector.create(80,80),false);
-const boxR=new RectangleCollider(engine.world,Matter.Vector.create(400,300),Matter.Vector.create(80,80),false);
-const boxS=new RectangleCollider(engine.world,Matter.Vector.create(500,700),Matter.Vector.create(1000,80),true);
-const circle=new CircleCollider(engine.world,Matter.Vector.create(400,150),10); 
-
-const boxes=[boxR1,boxR,boxS,circle];
-
-
-function renderAll(){
-    ctx.clearRect(0,0,Canvas.width,Canvas.height);
-    for(let i=0;i<boxes.length;i++)
-    boxes[i].draw(ctx);
-    window.requestAnimationFrame(renderAll);
-}
-renderAll();
+Renderer.render();//requestAnimationFrame 
+Player.init(engine.world);
