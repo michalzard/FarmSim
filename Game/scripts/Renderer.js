5e93ea786ab7a5cc5d742b8048d2f15883ac8487
MichalPlatko <miso.platko@gmail.com>
2021-11-07 20:11:55 +0100

    work in progress,renderer will hold data about where to render what

---
+++ Game/scripts/Renderer.js
@@ -0,0 +1,33 @@
+import Canvas from "./Canvas.js";
+window.onload=Canvas.init();//inits canvas after whole page loads
+const ctx=Canvas.ctx;
+
+/** temporary solution */
+export default class Renderer{
+    static layers={
+        env:[],
+        buildings:[],
+        entities:[],
+    };
+    
+    static render(){
+        ctx.clearRect(0,0,Canvas.width,Canvas.height);
+        for(let i=0;i<Renderer.layers.entities.length;i++){
+            const objectLayer=Renderer.layers.entities;
+            objectLayer[i].draw();
+        }
+        
+        window.requestAnimationFrame(Renderer.render);
+    }
+    static addToLayer(entity,layer){
+    const layers=Object.entries(Renderer.layers)
+    for(let i=0;i<layers.length;i++){
+        if(layers[i].includes(layer.toLowerCase())){
+        const foundLayer=layers[i];
+        const layerArray=foundLayer[1];
+        if(layerArray) layerArray.push(entity); 
+        }
+    }
+    }
+}
+
