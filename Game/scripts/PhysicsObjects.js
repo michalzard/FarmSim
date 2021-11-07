a1b0f86938d788a0c51c729ef82edbc486e7dc72
MichalPlatko <miso.platko@gmail.com>
2021-11-07 20:11:13 +0100

    added correct translation and rotation so now colliders rotate around its centerpoint

--- Game/scripts/PhysicsObjects.js
+++ Game/scripts/PhysicsObjects.js
@@ -18,8 +18,14 @@ export class RectangleCollider{
     draw(ctx){
         //needs to be rendered
         ctx.save();
-        ctx.strokeStyle="red";   
-        ctx.strokeRect(this.body.position.x-this.size.x/2,this.body.position.y-this.size.y/2,this.size.x,this.size.y);
+        ctx.strokeStyle="red";    
+        //Set the origin to the center of the rectangle
+        ctx.translate(this.body.position.x ,this.body.position.y );
+        //rotate 
+        ctx.rotate(this.body.angle);
+        //draw rect offset by half its size
+        ctx.strokeRect(-this.size.x/2,-this.size.y/2,this.size.x,this.size.y);
+        
         ctx.restore();
     }
     /**
@@ -49,7 +55,11 @@ export class CircleCollider{
         ctx.save();
         ctx.strokeStyle="red";
         ctx.beginPath();
-        ctx.arc(this.body.position.x-this.radius/2,this.body.position.y-this.radius/2,this.radius,0,Math.PI*2);
+        //Set the origin to the center of the rectangle
+        ctx.translate(this.body.position.x,this.body.position.y);
+        //rotate 
+        ctx.rotate(this.body.angle);
+        ctx.arc(0,0,this.radius,0,Math.PI*2);
         ctx.stroke();
         ctx.restore();
     }
@@ -63,40 +73,3 @@ export class CircleCollider{
     }
 }
 
-
-export class PolygonCollider{
-    constructor(worldToSpawn,position,numOfSides,radius,isStatic){
-        this.position=position || Vector.create(0,0);
-        this.sides=numOfSides || 5;
-        this.radius=radius || Vector.create(10,10);
-        this.isStatic=isStatic || false;
-        this.body=Bodies.polygon(this.position.x,this.position.y,this.sides,this.radius,{isStatic:this.isStatic});
-        /*
-        expected format to add object to physical world
-        Composite.add(engine.world, [boxA, boxB, ground]);
-        */
-        Composite.add(worldToSpawn,this.body);//World.add
-    }
-    draw(ctx){
-    //needs to be rendered
-    ctx.save();
-    ctx.beginPath();
-    ctx.strokeStyle = "red";   
-    ctx.moveTo (this.body.position.x-this.radius/2 +  this.radius * Math.cos(0),
-    this.body.position.y-this.radius/2 +  this.radius  *  Math.sin(0));          
-    for (let i=0;i<=this.sides;i++) {
-    ctx.lineTo (this.body.position.x-this.radius/2 +  this.radius * Math.cos(i * 2 * Math.PI / this.sides),
-    this.body.position.y-this.radius/2 +  this.radius * Math.sin(i * 2 * Math.PI / this.sides));
-    }
-    ctx.stroke();
-    ctx.restore();
-    }
-     /**
-     * Matter.Body.applyForce(body, position, force)
-     */
-    applyForce(force){
-    if(typeof force === "object"){
-    Body.applyForce(this.body,this.body.position,force);
-    }
-    }
-}
