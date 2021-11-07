672675910596be5e495c262131db650e522a7ff0
MichalPlatko <miso.platko@gmail.com>
2021-11-07 03:42:44 +0100

    canvas wrapper

---
+++ Game/scripts/Canvas.js
@@ -0,0 +1,21 @@
+export default class Canvas{
+    static width;
+    static height;
+    static element;
+    static ctx;
+    static init(){
+        Canvas.element=document.createElement('canvas');
+        Canvas.ctx=Canvas.element.getContext('2d');
+        //asign width and height
+        Canvas.width=Canvas.element.width=window.innerWidth;
+        Canvas.height=Canvas.element.height=window.innerHeight;
+        //style that gets rid of any padding and set default color to black
+        document.body.style="margin:0px;padding:0px;background-color:black;overflow:hidden";
+        document.body.appendChild(Canvas.element);
+    }
+    static resize(){
+
+    }
+
+}
+
