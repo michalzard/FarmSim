5a3e04410ed5c14b13ae7d87c0e46c5e860f5234
MichalPlatko <miso.platko@gmail.com>
2021-11-09 02:14:58 +0100

    added support for sprite atlases,ability to offset image cutout,offset frame that is being started with and more

---
+++ Game/scripts/SpritesheetHandler.js
@@ -0,0 +1,66 @@
+/**
+ * Everything related to sprites,spritesheets and image maninpulation
+ */
+import Renderer from "./Renderer.js";
+
+const Vector=Matter.Vector;
+
+
+export default class SpriteSheet{
+    constructor(imageSrc,size){
+    this.img=new Image();
+    this.img.src=imageSrc || null;
+    this.position=Vector.create(0,0);
+    this.size=size || Vector.create(32,32); //size of the texture
+    this.frameSize=Vector.create(64,64); //cut out
+    this.frameOffset=Vector.create(0,0);//adds aditional pixels to cut out
+    this.angle=0;
+    this.startingFrame=0;//offset to start on specific frame
+    this.currentFrame=this.startingFrame;//currently displayed frame
+    //cols,rows
+    this.frameColumns=1;
+    this.frameRows=1;
+    //inital setup for sprite looping
+    this.loopTimestep=100;
+    this.loop(this.loopTimestep);
+    
+    Renderer.addToLayer(this,"debug");
+    }
+
+    draw(ctx){
+    //update rows and columns
+    const col=this.currentFrame % this.frameColumns;
+    const row=Math.floor(this.currentFrame/this.frameColumns);
+    ctx.save();
+    //Set the origin to the center of the rectangle
+    ctx.translate(this.position.x ,this.position.y);
+    //rotate 
+    ctx.rotate(this.angle);
+    //draw rect offset by half its size
+    ctx.drawImage(this.img,col * this.frameSize.x+this.frameOffset.x,
+    row * this.frameSize.y + this.frameOffset.y,
+    this.frameSize.x,this.frameSize.y,
+    -this.size.x/2,-this.size.y/2
+    ,this.size.x,this.size.y);
+    
+    ctx.restore();
+    
+    }
+
+    update(position,angle){
+        this.position=position;
+        this.angle=angle;
+    }
+
+    loop(timestep){
+    setInterval(()=>{
+    this.currentFrame++;
+    const lastFrame=this.frameColumns*this.frameRows - 1;
+    if(this.currentFrame > lastFrame){
+    this.currentFrame=this.startingFrame;
+    }
+    },timestep);
+    }
+
+}
+
