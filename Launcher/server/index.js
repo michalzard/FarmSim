93cfd8569c22de7566e64e4a605d0d2755caef72
MichalPlatko <miso.platko@gmail.com>
2021-12-07 15:56:51 +0100

    web server with /map endpoint where map data are being saved/loaded from

--- Launcher/server/index.js
+++ Launcher/server/index.js
@@ -6,11 +6,30 @@ const path=require('path');
 app.use(express.json());
 
 
-app.post('/',(req,res)=>{
+app.post('/map',(req,res)=>{
     const mapDataArray=req.body;
     saveMapFile('testing',mapDataArray);
-    res.send({message:'Response from web server'});
+    res.send({message:'Map Data posted'});
 });
+
+app.get('/map',(req,res)=>{
+    const {name} = req.query;
+    // console.log(readMapFile(name));
+    if(name!==null){
+    fs.readFile(path.join(__dirname,`/saves/${name}.json`),'utf8',function(err,data){
+        if(err)console.log(err);
+
+        const size = Buffer.byteLength(JSON.stringify(data)) /1024; //kilobytes
+        //data is contents of file
+         if(data){
+             res.send({message:`${name}.json was loaded, ${size.toFixed(3)}kb`,mapData:data});
+         }else res.send({message:'Map file wasnt loaded correctly'});
+         
+     });
+    }
+    else res.send({message:`Unable to find specified map file`});
+});
+
 
 app.listen(3000,()=>{
     console.log('web server ~3000');
@@ -24,9 +43,3 @@ fs.writeFile(path.join(__dirname,`/saves/${fileName}.json`),JSON.stringify(data)
 
 }
 
-function readMapFile(fileName){
-fs.readFile(path.join(__dirname,`/saves/${fileName}.json`),'utf8',function(err,data){
-   if(err)console.log(err);
-    //data is contents of file
-});
-}
