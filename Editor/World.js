8e80a03811b79ecc0bd660359b655384b57af363
MichalPlatko <miso.platko@gmail.com>
2021-12-09 21:54:16 +0100

    made editor into gameobject

--- Editor/World.js
+++ Editor/World.js
@@ -1,123 +1,92 @@
 const Vector=Matter.Vector;
 import {Tile, TilePattern} from "../Game/scripts/Objects.js";
-import SpriteSheet,{Textures} from "../Game/scripts/SpritesheetHandler.js";
+import SpriteSheet, {Textures} from "../Game/scripts/SpritesheetHandler.js";
 import Canvas, { Mouse } from "../Game/scripts/Canvas.js";
 import Renderer from "../Game/scripts/Renderer.js";
+import { GameObject } from "../Game/scripts/GameObject.js";
 
 //debug only
 
-const t = new Tile("grass", Textures.grass);
-const t2 = new Tile("inv", Textures.inv);
+const t = new Tile("grass",new SpriteSheet(Textures.grass));
+const t2 = new Tile("inv", new SpriteSheet(Textures.inv));
 //
 
-export default class WorldEditor {
-
-  static availableTiles = [];
-  static defaultAvailableTiles = [];
-  static maxSlots = 36;
-  static slots = {
-    lastSelectedTileIndex: -1, //default to -1 since there's tile usually at 0th index position
-  };
-  static currentPage = 1;
-  static themeColors = {
-    blue: "#7289da",
-    lightgray: "#99aab5",
-    gray: "#313639",
-    darkgray: "#2c2f33",
-    black: "#23272a"
-  };
-  static keybinds = {
-    open: 'F1',
-    changePageUp: 'ArrowUp',
-    changePageDown: 'ArrowDown',
-    undo : 'z',
-    redo: 'y',
-  };
+class WorldEditor extends GameObject{
+  constructor(){
+    super();
+    this.active=false;
+    this.availableTiles = [];
+    this.defaultAvailableTiles = [];
+    this.maxSlots = 36;
+    this.slots = {
+      lastSelectedTileIndex: -1, //default to -1 since there's tile usually at 0th index position
+    };
+    this.currentPage = 1;
+    this.themeColors = {
+      blue: "#7289da",
+      lightgray: "#99aab5",
+      gray: "#313639",
+      darkgray: "#2c2f33",
+      black: "#23272a"
+    };
+    this.keybinds = {
+      open: 'F1',
+      changePageUp: 'ArrowUp',
+      changePageDown: 'ArrowDown',
+      undo : 'z',
+      redo: 'y',
+    };
+    this.input = document.createElement('input');
+    this.history = {
+      actions:[],
+      undoneActions:[],
+      actionLimit:30,
+      addAction:(type,tile)=>{
+      //TODO : Debug this shit cuz its bugging adding and removing somehow
+      if(this.history.actions.length<this.history.actionLimit) this.history.actions.push({type,tile});
+      else {this.history.actions.shift();this.history.actions.push({type,tile});}
+      },
+      handleUndoAndRedo:(e)=>{this.history.undo(e);this.history.redo(e);},
+      undo:(e)=>{
+        // CTRL + Z
+        if(e.ctrlKey && e.key===this.keybinds.undo){
+        //undoes last action,returns tiles back to world    
+        for(let i=this.history.actions.length-1;i>0;i--){
+          const actions=this.history.actions[i]
+          if(actions.type==='remove') {
+          const lastRemovedTile=this.history.actions[i].tile;
+          if(lastRemovedTile){
   
-  static history = {
-    actions:[],
-    undoneActions:[],
-    actionLimit:30,
-    addAction:(type,tile)=>{
-    //TODO : Debug this shit cuz its bugging adding and removing somehow
-    if(this.history.actions.length<this.history.actionLimit) this.history.actions.push({type,tile});
-    else {this.history.actions.shift();this.history.actions.push({type,tile});}
-    },
-    handleUndoAndRedo:(e)=>{this.history.undo(e);this.history.redo(e);},
-    undo:(e)=>{
-      // CTRL + Z
-      if(e.ctrlKey && e.key===this.keybinds.undo){
-      //undoes last action,returns tiles back to world    
-      for(let i=this.history.actions.length-1;i>0;i--){
-        const actions=this.history.actions[i]
-        if(actions.type==='remove') {
-        const lastRemovedTile=this.history.actions[i].tile;
-        if(lastRemovedTile){
-
-          if(this.history.undoneActions.length<this.history.actionLimit) this.history.undoneActions.push(actions);
-          else {this.history.undoneActions.shift();this.history.undoneActions.push(actions);}
-          
-          Renderer.addToLayer(lastRemovedTile,lastRemovedTile.desiredLayer);
-        }
-        else return;
-        }
-      }}
-    },
-    redo:(e)=>{
-      // CTRL + Y
-      if(e.ctrlKey &&  e.key===this.keybinds.redo){
-        //redos last undo,removes undone tiles from world    
-        for(let i=0;i<this.history.undoneActions.length;i++){
-          const undoes=this.history.undoneActions[i];
-          const undoneTiles=undoes.tile;
-          
-          if(undoneTiles)Renderer.removeFromLayer(undoneTiles,undoneTiles.desiredLayer);
-        }
-      }
-    },
-  };
-
-  static tools={
-    rectangle:{
-      tileMode:'tile',//default mode : tile , is switched to pattern when using rectangle tool
-      originPoint:null, // first time user clicks on specific spot will asign position to origin point
-      //from o.point they can click second time to establish width/height of pattern 
-      endPoint:null,
-      clickCount:0,
-      switchModes:(e)=>{
-      if(e.shiftKey && this.tools.rectangle.tileMode==='tile' && this.slots.lastSelectedTileIndex>=0) this.tools.rectangle.tileMode='pattern';
-      else this.tools.rectangle.tileMode='tile';
+            if(this.history.undoneActions.length<this.history.actionLimit) this.history.undoneActions.push(actions);
+            else {this.history.undoneActions.shift();this.history.undoneActions.push(actions);}
+            
+            Renderer.addToLayer(lastRemovedTile,lastRemovedTile.desiredLayer);
+          }
+          else return;
+          }
+        }}
       },
-      setOriginAndEnd(){
-    
-        if(this.clickCount==0){
-          //set origin and add to click counter
-          this.originPoint=WorldEditor.translateToGrid(Mouse.position,Vector.create(40,40));
-          this.clickCount=1;
+      redo:(e)=>{
+        // CTRL + Y
+        if(e.ctrlKey &&  e.key===this.keybinds.redo){
+          //redos last undo,removes undone tiles from world    
+          for(let i=0;i<this.history.undoneActions.length;i++){
+            const undoes=this.history.undoneActions[i];
+            const undoneTiles=undoes.tile;
+            
+            if(undoneTiles)Renderer.removeFromLayer(undoneTiles,undoneTiles.desiredLayer);
+          }
         }
-        else if(this.clickCount===1){
-          //set endpoint and reset click counter
-          this.endPoint=WorldEditor.translateToGrid(Mouse.position,Vector.create(40,40));
-          this.clickCount=2;
-        }
-        else{
-          //if click count is more than 1 means its being clicked third time so it resets counter and positions to assign new ones
-          this.clickCount=0;
-          this.originPoint=null;
-          this.endPoint=null;
-        }
-        console.log(this.clickCount,this.originPoint,this.endPoint);
       },
-    }
+    };
+
   }
-  static opened = false;
-  static input = document.createElement('input');
   /**
    *  initializes spritesheets so textures can be used
    * @param {*} sprite can pass single sprite or array of sprites  
    * @param {*} category tag
    */
-  static addTileTypes(tile, category) {
+  addTileTypes(tile, category) {
     this.availableTiles.push({tile,category});
     this.defaultAvailableTiles.push({tile,category});
   }
@@ -125,39 +94,33 @@ export default class WorldEditor {
   /**
    * shows currently "equipped" tile for editing
    */
-  static displayGhostTile(ctx) {
+  displayGhostTile(ctx) {
     //shows current selection of tile
     if (this.slots.lastSelectedTileIndex >= 0) {
       ctx.save();
       ctx.globalAlpha = 0.4;
       const currentTile = this.availableTiles[this.slots.lastSelectedTileIndex];
-      if(currentTile && this.tools.rectangle.tileMode==='tile'){
+      if(currentTile){
       currentTile.tile.texture.position = Mouse.position;
       currentTile.tile.texture.draw(ctx);
       }
-      // if(this.tools.rectangle.originPoint!==null && this.tools.rectangle.endPoint!==null ){
-      //   const rect=this.tools.rectangle;
-      //   ctx.strokeStyle='red';
-      //   ctx.strokeRect(rect.originPoint.x,rect.originPoint.y,rect.endPoint.x-rect.originPoint.x,rect.endPoint.y-rect.originPoint.y);
-        
-      // }
       ctx.restore();
     }
   }
   /** position of said tile is needed,gridSize should be put in place depending on tile*/
-  static translateToGrid(position,gridSize) {
+  translateToGrid(position,gridSize) {
     return Vector.create(Math.round(position.x / gridSize.x) * gridSize.x, Math.round(position.y / gridSize.y) * gridSize.y);
   }
-  static handleOpen(e) {
+  handleOpen(e) {
     if (e.key === this.keybinds.open) {
       this.input.style = "display:none";
-      this.opened = !this.opened;
+      this.setActive(!this.active);
     }
   }
-  static maxPageCount() {
+  maxPageCount() {
     return Math.ceil(this.availableTiles.length / this.maxSlots);
   }
-  static changeCurrentPage(e) {
+  changeCurrentPage(e) {
     if (e.key === this.keybinds.changePageUp && this.currentPage > 1) {
       this.currentPage--;
     } else if (e.key === this.keybinds.changePageDown && this.currentPage < this.maxPageCount()) {
@@ -166,14 +129,14 @@ export default class WorldEditor {
   }
 
   //ADD,REMOVE FROM LAYER
-  static addTileToWorld() {
+  addTileToWorld() {
     //check if there's tile object in assigned tile 
     if (this.slots.lastSelectedTileIndex >= 0 && this.availableTiles[this.slots.lastSelectedTileIndex]!==undefined) {
     const copy = this.availableTiles[this.slots.lastSelectedTileIndex].tile;
     const worldTile = new Tile(copy.label, copy.texture, Mouse.position);
-    worldTile.position= this.translateToGrid(Mouse.position,worldTile.size);
+    worldTile.transform.position= this.translateToGrid(Mouse.position,worldTile.transform.size);
 
-     let desiredLayerName=null;
+    let desiredLayerName=null;
     let isLastTileUnderMouse=false;
     if(this.availableTiles[this.slots.lastSelectedTileIndex].tile) {
     desiredLayerName=this.availableTiles[this.slots.lastSelectedTileIndex].tile.desiredLayer;
@@ -181,8 +144,8 @@ export default class WorldEditor {
     
     for(let i=0;i<Renderer.getLayer(desiredLayerName).length;i++){
     const toCheck=Renderer.getLayer(desiredLayerName)[i];
-    const offsettedPosition=Vector.create(toCheck.position.x-toCheck.size.x/2,toCheck.position.y-toCheck.size.y/2);
-    isLastTileUnderMouse=Mouse.checkBoxCollision(offsettedPosition,toCheck.size);  
+    const offsettedPosition=Vector.create(toCheck.transform.position.x-toCheck.transform.size.x/2,toCheck.transform.position.y-toCheck.transform.size.y/2);
+    isLastTileUnderMouse=Mouse.checkBoxCollision(offsettedPosition,toCheck.transform.size);  
     }
     
     if(worldTile && !isLastTileUnderMouse){
@@ -192,7 +155,7 @@ export default class WorldEditor {
     }
     else return;
   }
-  static removeTileFromWorld(){
+  removeTileFromWorld(){
     let desiredLayerName=null;
     if(this.availableTiles[this.slots.lastSelectedTileIndex]!==undefined) {
     desiredLayerName=this.availableTiles[this.slots.lastSelectedTileIndex].tile.desiredLayer;
@@ -210,7 +173,7 @@ export default class WorldEditor {
 }
 
 
-static draw(ctx) {
+draw(ctx) {
   const filteredTiles = this.availableTiles.filter(tile => {
     if (tile.category.includes(this.input.value)) return tile;
   });
@@ -282,7 +245,7 @@ static draw(ctx) {
   //   }
   // }
 
-  static saveWorld(e){
+  saveWorld(e){
     if(e.ctrlKey && e.key==="s"){
     console.log('World Saved')
     const allLayers=Object.entries(Renderer.layers).filter(el=>{
@@ -307,7 +270,7 @@ static draw(ctx) {
   })
   }
   }
-  static loadWorld(worldName){
+  loadWorld(worldName){
 
     fetch(`http:localhost:3000/map?name=${worldName}`).then(data=>data.json()).then(data=>{
       const {message,mapData} = data;
@@ -318,25 +281,26 @@ static draw(ctx) {
         for(let j=0;j<map.data.length;j++){
           const mData=map.data[j];//tiles and other objects
 
-       
           //using to lowercase so that it can correctly lookup needed texture
-          const tile=new Tile(mData.label,Textures[mData.label.toLowerCase()],mData.position);
+          const texture=new SpriteSheet(Textures[mData.label.toLowerCase()]);
+          const tile=new Tile(mData.label,texture,mData.position);
           Renderer.addToLayer(tile,map.layer);
-        
-
         }
       }
     });
   }
   
 }
-document.body.append(WorldEditor.input);
+export const worldEdit=new WorldEditor();
+
+worldEdit.input.style="display:none";//hide it by default
+document.body.append(worldEdit.input);
 
 document.addEventListener('keydown', (e) => {
-  WorldEditor.handleOpen(e);
-  WorldEditor.changeCurrentPage(e);
-  WorldEditor.history.handleUndoAndRedo(e);
-  WorldEditor.saveWorld(e);
+  worldEdit.handleOpen(e);
+  worldEdit.changeCurrentPage(e);
+  worldEdit.history.handleUndoAndRedo(e);
+  worldEdit.saveWorld(e);
 });
 
 // document.addEventListener('mousedown',(e)=>{
@@ -346,9 +310,7 @@ document.addEventListener('keydown', (e) => {
  * TODO: Rectangle tool that will select area and place pattern in that size
  */
 
+ worldEdit.addTileTypes(t, "grass");
+ worldEdit.addTileTypes(t2, "inventory");
 
-
-WorldEditor.addTileTypes(t, "grass");
-WorldEditor.addTileTypes(t2, "inventory");
-
-WorldEditor.loadWorld('testing');
+ worldEdit.loadWorld('testing');
