18521d657ab4d91a12b3ace2723ef5bb76681ec1
MichalPlatko <miso.platko@gmail.com>
2021-12-16 02:30:00 +0100

    extends go

--- Game/scripts/SpritesheetHandler.js
+++ Game/scripts/SpritesheetHandler.js
@@ -3,20 +3,19 @@
  */
 const Vector=Matter.Vector;
 
+import { GameObject } from "./GameObject.js";
 
-export default class SpriteSheet{
+export default class SpriteSheet extends GameObject{
     constructor(imageSrc){
+    super();
+    this.label="Sprite Atlas";
     this.img=new Image();
     this.img.src=imageSrc || null;
-    this.position=Vector.create(0,0);
-    this.size=Vector.create(40,40); //size of the texture
     this.frameSize=Vector.create(16,16); //cut out
     this.frameOffset=Vector.create(0,0);//adds aditional pixels to cut out
     this.angle=0;
     this.startingFrame=0;//offset to start on specific frame
     this.currentFrame=this.startingFrame;//currently displayed frame
-    //
-    this.scale=Vector.create(1,1);
     //cols,rows
     this.frameColumns=1;
     this.frameRows=1;
@@ -31,24 +30,24 @@ export default class SpriteSheet{
     const row=Math.floor(this.currentFrame/this.frameColumns);
     ctx.save();
     //Set the origin to the center of the rectangle
-    ctx.translate(this.position.x ,this.position.y);
+    ctx.translate(this.transform.position.x ,this.transform.position.y);
     //rotate 
-    ctx.rotate(this.angle);
-    ctx.scale(this.scale.x,this.scale.y);
+    ctx.rotate(this.transform.angle);
+    ctx.scale(this.transform.scale.x,this.transform.scale.y);
     //draw rect offset by half its size
     ctx.drawImage(this.img,col * this.frameSize.x+this.frameOffset.x,
     row * this.frameSize.y + this.frameOffset.y,
     this.frameSize.x,this.frameSize.y,
-    -this.size.x/2,-this.size.y/2
-    ,this.size.x,this.size.y);
+    -this.transform.size.x/2,-this.transform.size.y/2
+    ,this.transform.size.x,this.transform.size.y);
     
     ctx.restore();
     
     }
 
     update(position,angle){
-        this.position=position;
-        this.angle=angle;
+        this.transform.position=position;
+        this.transform.angle=angle;
     }
 
     loop(timestep){
@@ -68,5 +67,4 @@ export default class SpriteSheet{
 export const Textures={
     grass : "../Game/assets/grassdirt.png",
     inv : "../Game/assets/inventory.png",
-    invui : '../Game/assets/inventoryUI.png',
 }
